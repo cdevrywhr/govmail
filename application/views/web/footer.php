9712e58df22a90f662d9277b139dec02424f366c
pulangsore <robyjulian212@mhs.mdp.ac.id>
2020-10-14 12:08:15 +0700

    first

---
+++ application/views/web/footer.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+<div class="footer text-muted text-center">
+	&copy; 2020. <a href="">GOV-MAIL</a>
+</div>
+<!-- /footer -->
+
+</div>
+<!-- /content area -->
+
+</div>
+<!-- /main content -->
+
+</div>
+<!-- /page content -->
+
+</div>
+<!-- /page container -->
+
+</body>
+
+</html>
